917766d7f57b4cd1760fd90f88729e9ec694323a
Kao, Bai Chien <bai.chien.kao@accenture.com>
2022-10-07 11:45:44 -0400

    first commit

---
+++ src/main/java/com/sbomtest/SbomTest2/App.java
@@ -0,0 +1,17 @@
+package com.sbomtest.SbomTest2;
+
+import org.jboss.resteasy.*;
+import com.xxl.job.core.enums.*;
+import io.atomix.cluster.discovery.*;
+import scala.Int$;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
