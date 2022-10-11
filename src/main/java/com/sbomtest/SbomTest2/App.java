718d4e0853e4fe0f83f3fc2f960068d3d29f1768
Kao, Bai Chien <bai.chien.kao@accenture.com>
2022-10-11 16:25:09 -0400

    Updated 10112022

--- src/main/java/com/sbomtest/SbomTest2/App.java
+++ src/main/java/com/sbomtest/SbomTest2/App.java
@@ -1,9 +1,16 @@
 package com.sbomtest.SbomTest2;
 
-import org.jboss.resteasy.*;
-import com.xxl.job.core.enums.*;
-import io.atomix.cluster.discovery.*;
+import java.io.File;
+import java.util.List;
+
+import org.jboss.resteasy.core.config.DefaultConfigurationFactory;
+
+import com.google.common.base.Converter;
+import com.google.common.base.Enums;
+import com.xxl.job.core.enums.RegistryConfig;
+import io.atomix.core.AtomixConfig;
 import scala.Int$;
+
 /**
  * Hello world!
  *
@@ -12,6 +19,17 @@ public class App
 {
     public static void main( String[] args )
     {
+    	int TimeOut = RegistryConfig.DEAD_TIMEOUT;
+    	DefaultConfigurationFactory DefConfigFactory = new DefaultConfigurationFactory();
+    	String Sbom = new String(DefConfigFactory.toString());
         System.out.println( "Hello World!" );
     }
+    
+    public static AtomixConfig config(Object classLoader, List<File> files) {
+    	 return config(Thread.currentThread().getContextClassLoader(), files);
+    }
+    
+    public static <T extends Enum<T>> Converter<String,T> parse(Class<T> clazz) {
+        return Enums.stringConverter(clazz);
+     }
 }
