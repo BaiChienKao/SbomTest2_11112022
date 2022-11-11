9fd813d97a64dd5f5df1699d5e9b604ff125e44e
Bai Chen Kao <bai.chien.kao@amac02fd4pnq05n.mynetworksettings.com>
2022-11-11 16:25:56 -0500

    first commit

--- src/main/java/com/sbomtest/SbomTest2/App.java
+++ src/main/java/com/sbomtest/SbomTest2/App.java
@@ -1,15 +1,20 @@
 package com.sbomtest.SbomTest2;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.jboss.resteasy.core.config.DefaultConfigurationFactory;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 
 import com.google.common.base.Converter;
 import com.google.common.base.Enums;
 import com.xxl.job.core.enums.RegistryConfig;
 import io.atomix.core.AtomixConfig;
 import scala.Int$;
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.deeplearning4j.core.datasets.test.TestDataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 
 /**
  * Hello world!
@@ -17,11 +22,18 @@ import scala.Int$;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
     	int TimeOut = RegistryConfig.DEAD_TIMEOUT;
     	DefaultConfigurationFactory DefConfigFactory = new DefaultConfigurationFactory();
     	String Sbom = new String(DefConfigFactory.toString());
+    	RemoteIpFilter filter = new RemoteIpFilter();
+    	
+    	int batchSize = 0;
+		int rngSeed = 0;
+		DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
+		TestDataSetIterator testDatasetIterator = new TestDataSetIterator(mnistTest);
+    	
         System.out.println( "Hello World!" );
     }
     
